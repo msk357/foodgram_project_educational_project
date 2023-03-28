1f510198b58b009718604af64b38dcfd86f800f8
msk357 <108631219+msk357@users.noreply.github.com>
2023-03-28 11:23:25 +0300

    fix order by pub_date

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -127,7 +127,6 @@ class RecipeViewSet(ModelViewSet, CreateDelViewMixin):
     permission_classes = [AuthorStaffOrReadOnly]
     add_serializer = CropRecipeSerializer
     pagination_class = FoodgramPagination
-    ordering = ('-pub_date',)
 
     def get_queryset(self):
         """Получает queryset в соответствии с запросом.
@@ -159,7 +158,7 @@ class RecipeViewSet(ModelViewSet, CreateDelViewMixin):
             queryset = queryset.filter(in_favorites__user=self.request.user)
         if is_favorit in Tuples.SYMBOL_FALSE_SEARCH.value:
             queryset = queryset.exclude(in_favorites__user=self.request.user)
-        return queryset
+        return queryset.order_by('-pub_date')
 
     @action(
         methods=["get", "post", "delete"],
