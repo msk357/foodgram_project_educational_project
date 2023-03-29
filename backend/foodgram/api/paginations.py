58b988f9f5e9365121ad2889bec248f82477c801
msk357 <108631219+msk357@users.noreply.github.com>
2023-03-29 22:07:18 +0300

    fix order by pub_date

--- backend/foodgram/api/paginations.py
+++ backend/foodgram/api/paginations.py
@@ -2,7 +2,7 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class PageLimitPagination(PageNumberPagination):
-    page_size = 'limit'
+    page_size_query_param = 'limit'
 
 class SubPagination(PageNumberPagination):
     page_size_query_param = 3
